90644cb93d376e54e96c4082f5502654a6b62283
Hbin <@email.com>
2024-11-09 17:22:29 +0800

    首页改名0.2

---
+++ index/js/index.js
@@ -0,0 +1,15 @@
+let a = document.querySelectorAll('a')
+
+a.forEach(function (e) {
+  let img=e.querySelector('.img')
+  console.log(e);
+  e.addEventListener('mouseover', function () {
+    img.style.width = '400px'
+    img.style.height = '400px'
+  })
+  e.addEventListener('mouseout', function () {
+    img.style.width = '400px'
+    img.style.height = '0'
+  })
+  
+})
